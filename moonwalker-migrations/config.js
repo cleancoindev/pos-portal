d4a643bfdcb92b31fc52e18d1583ca6a4dbec342
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2021-03-20 17:59:30 -0700

    Update config.js
    Kind people, please hide your keys.  They do not belong in public thanks.

--- moonwalker-migrations/config.js
+++ moonwalker-migrations/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mnemonic: process.env.MNEMONIC || 'clock radar mass judge dismiss just intact mind resemble fringe diary casino',
+  mnemonic: process.env.MNEMONIC || '<insert fake private mnemonic for wallet here>',
   from: process.env.FROM || '0x9fB29AAc15b9A4B7F17c3385939b007540f4d791',
   url: process.env.PROVIDER_URL || 'http://localhost:9545',
   gasPrice: process.env.GAS_PRICE || '45000000000', // 45 gwei
